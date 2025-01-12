22be35569ae2913f55d622d27c231a1229eac97e
Eric Bryan <eb@erbiker.dev>
2025-01-11 21:53:28 -0800

    create example post route for app creation

--- app/api/apps/route.ts
+++ app/api/apps/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { PrismaClient } from '@prisma/client';
 import { type NextRequest, NextResponse } from 'next/server';
 
@@ -6,4 +8,18 @@ const prisma = new PrismaClient();
 export async function GET(request: NextRequest) {
   const apps = await prisma.app.findMany();
   return NextResponse.json(apps);
+}
+
+export async function POST(request: NextRequest) {
+  const app = await prisma.app.create({
+    data: {
+      name: 'New App',
+      owner: {
+        connect: {
+          id: 'user_id',
+        },
+      },
+    },
+  });
+  return NextResponse.json(app);
 }
